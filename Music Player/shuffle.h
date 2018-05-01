caa14888ea32d82b5c89a9020ee569bcb1d3e5ed
Navroop Singh Bajwa <navroop.bajwa@yahoo.com>
2018-05-01 18:20:43 +0530

    searching improved

---
+++ Music Player/shuffle.h
@@ -0,0 +1,53 @@
+//
+//  shuffle.h
+//  Music Player
+//
+//  Created by Navroop Singh Bajwa on 01/05/18.
+//  Copyright © 2018 Navroop Singh Bajwa. All rights reserved.
+//
+
+#ifndef shuffle_h
+#define shuffle_h
+#include<iostream>
+using namespace std;
+
+class shuffle_songs{
+private:
+    int *pointer;
+    int size=-1;
+    int counter=-1;
+public:
+    shuffle_songs(int number_of_songs){
+        pointer=new int[number_of_songs];
+        for(int i=0;i<number_of_songs;i++){
+            pointer[i]=0;
+        }
+        size=number_of_songs;
+        counter=0;
+    }
+    void display(){
+        for(int i=0;i<size;i++){
+            cout<<pointer[i]<<" ";
+        }cout<<endl;
+    }
+    int next_song(){
+        while(true){
+            int rand_value=random()%size;
+            if(counter<size){
+                if( rand_value<size &&rand_value>=0 && pointer[rand_value]==0  ){
+                    pointer[rand_value]=1;
+                    counter++;
+                    return rand_value;
+                }
+            }else{
+                for(int i=0;i<size;i++){
+                    pointer[i]=0;
+                }
+                counter=0;
+            }
+            
+        }
+    }
+};
+
+#endif /* shuffle_h */
---
+++ main.cpp
@@ -0,0 +1,37 @@
+//
+//  main.cpp
+//  Music Player
+//
+//  Created by Navroop Singh Bajwa on 21/04/18.
+//  Copyright © 2018 Navroop Singh Bajwa. All rights reserved.
+//
+#include<iostream>
+using namespace std;
+#include "songs.h"
+
+int main(int argc, const char * argv[]) {
+    songs obj;
+    obj.song_read_from_file();
+    obj.display_all_songs();
+    
+    
+    //obj.search_song.display();
+    //obj.search_song.display_unique_ids(obj.search_song.search_for_this_string("geet"));
+    //obj.search_artist.display();
+    int t;
+    cout<<"Input no of queries : ";
+    cin>>t;
+    while(t--){
+        int choice;
+        cout<<"1)search for singer\n2)search for song name\n";
+        cin>>choice;
+        string temp;
+        cin>>temp;
+        if(choice==1){
+            obj.display_specific_from_ids(obj.search_artist.search_for_this_string(temp));
+        }else{
+            obj.display_specific_from_ids(obj.search_song.search_for_this_string(temp));
+        }
+    }
+    return 0;
+}
